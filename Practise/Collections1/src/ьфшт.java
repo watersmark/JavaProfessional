d2b89baa3feafa7693f0e38c7c81a57939318fdc
watersmark <watersmark@mail.ru>
2020-10-30 15:51:51 +0300

    ArrayList2

---
+++ Practise/Collections1/src/ьфшт.java
@@ -0,0 +1,7 @@
+public class ьфшт {
+    public static void main(String[] args) {
+        Object[] DEFAULTCAPACITY_EMPTY_ELEMENTDATA = {};
+        System.out.println(DEFAULTCAPACITY_EMPTY_ELEMENTDATA.length);
+
+    }
+}
